fbed802150568cfa0b4502e95d2c889a1dcda425
zhangxiaodang <961590849@qq.com>
2020-04-09 09:17:28 +0800

    修改redis驱动

--- src/main/java/com/frank/gramturmq/GramtuRmqApplication.java
+++ src/main/java/com/frank/gramturmq/GramtuRmqApplication.java
@@ -1,10 +1,12 @@
 package com.frank.gramturmq;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
+@EnableRabbit
 @SpringBootApplication
 public class GramtuRmqApplication {
 
